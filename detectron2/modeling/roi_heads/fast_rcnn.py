246705a0a7389153c75de0f9028897a6aa12b946
M. A. <mohaliyeu@gmail.com>
2024-07-08 17:30:54 +0300

    variable name all_classes changed to scores_all

--- detectron2/modeling/roi_heads/fast_rcnn.py
+++ detectron2/modeling/roi_heads/fast_rcnn.py
@@ -171,7 +171,7 @@ def fast_rcnn_inference_single_image(
     result.scores = scores
     result.pred_classes = filter_inds[:, 1]
     # print(result.scores)
-    result.all_classes = all_scores[filter_inds[:,0]]
+    result.scores_all = all_scores[filter_inds[:,0]]
     return result, filter_inds[:, 0]
 
 
